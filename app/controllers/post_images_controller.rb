1b6e580970a01a8ea31a4c0f135891b0efccce6d
katsuma0728 <126679078+katsuma0728@users.noreply.github.com>
2023-03-16 13:46:45 +0000

    application2-15

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,6 +1,6 @@
 class PostImagesController < ApplicationController
   def new
-    @post_images = PostImage.new
+    @post_image = PostImage.new
   end
 
   def create
@@ -11,19 +11,24 @@ class PostImagesController < ApplicationController
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
-  
+
   def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to post_images_path
   end
-  
+
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
-  
+
+
 end
